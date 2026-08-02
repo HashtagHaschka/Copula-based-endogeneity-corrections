ba4784d1ed20135d976ef16b621d88f4261e96c4
Rouven E. Haschka <201503937+HashtagHaschka@users.noreply.github.com>
2026-08-02 11:33:01 +0200

    Update Copreg_ima.R

--- Copreg_ima.R
+++ Copreg_ima.R
@@ -12,6 +12,39 @@ if (!exists(".copreg_fit"))
 
 
 ## -----------------------------------------------------------------------------
+##  Copula-based endogeneity corrections in R
+##  https://github.com/HashtagHaschka/Copula-based-endogeneity-corrections
+##
+##  Copyright (C) 2026 Rouven E. Haschka
+##  ORCID: https://orcid.org/0000-0002-2916-9745
+##
+##  If this code contributes to work you publish, please cite the software
+##
+##    Haschka, R. E. (2026). Copula-based endogeneity corrections in R.
+##    https://github.com/HashtagHaschka/Copula-based-endogeneity-corrections
+##
+##  and the paper this estimator implements
+##
+##    Haschka, R. E. (2025). Robustness of copula-correction models in causal
+##      analysis: Exploiting between-regressor correlation. IMA Journal of
+##      Management Mathematics 36(1), 161-180.
+##
+##  This program is free software: you can redistribute it and/or modify it
+##  under the terms of the GNU General Public License as published by the Free
+##  Software Foundation, either version 3 of the License, or (at your option)
+##  any later version.
+##
+##  This program is distributed in the hope that it will be useful, but WITHOUT
+##  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+##  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+##  more details.
+##
+##  You should have received a copy of the GNU General Public License along
+##  with this program.  If not, see <https://www.gnu.org/licenses/>.
+## -----------------------------------------------------------------------------
+
+
+## -----------------------------------------------------------------------------
 ##  Copula term constructor
 ## -----------------------------------------------------------------------------
 ##  Procedure of Section 3.2:
